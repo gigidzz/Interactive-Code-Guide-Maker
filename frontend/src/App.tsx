e8213df8175c557d80265bfb2e48b75d3d280583
lukakh1 <lukakhimshiashvili4@gmail.com>
2025-06-01 23:38:25 +0400

    frontend configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='bg-green-500 flex h-full'>
+      <p>hello interactive code guide maker</p>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+    server: {
+    port: 3001,
+    host: true,
+    open: true,
+    strictPort: true
+  }
+})
